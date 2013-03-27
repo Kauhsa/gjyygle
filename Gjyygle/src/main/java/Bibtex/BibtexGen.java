88bc01486f749725e46bcc7e0bc02c168c137548
Tuukka T Peuraniemi <peuranie@dhp-12.cs.helsinki.fi>
2013-03-27 17:16:15 +0200

    ArticleEntry korjattu

---
+++ Gjyygle/src/main/java/Bibtex/BibtexGen.java
@@ -0,0 +1,50 @@
+package Bibtex;
+
+import gjyygle.BibtexTietokanta;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.EnumMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BibtexGen {
+
+    private List<BibtexEntry> lista;
+    
+    public BibtexGen (BibtexTietokanta db) {
+        lista = db.listaaArtikkelit();
+    }
+// SAMPLE BIBTEXFILUSTA
+//    @ARTICLE{Bailey,
+//   author = "D. H. Bailey and P. N. Swarztrauber",
+//   title = "The fractional {F}ourier transform and applications",
+//   journal = "SIAM Rev.",
+//   volume = 33,
+//   number = 3,
+//   pages = "389--404",
+//   year = 1991
+//   }
+    public void generate() {
+//        PrintWriter tiedot = null;
+//        try {
+//            tiedot = new PrintWriter(new File("bibtex.bib"));
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(BibtexGen.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        for (BibtexEntry entry : lista) {
+//            EnumMap<BibtexField, String> map = entry.getAllValues();
+//            for ()
+//            tiedot.println("@ARTICLE{");
+//            tiedot.close();
+//        }
+    }
+
+    
+//    public static void main(String[] args) {
+//        BibtexGen gen = new BibtexGen(t);
+//        gen.generate();
+//    }
+}
