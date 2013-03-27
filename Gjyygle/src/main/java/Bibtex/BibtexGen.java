2d4f971a39e16d911bbbb8c80e6dfb9c2532580c
Tuukka T Peuraniemi <peuranie@dhp-12.cs.helsinki.fi>
2013-03-27 18:10:27 +0200

    BibtexGen fiksausta

--- Gjyygle/src/main/java/Bibtex/BibtexGen.java
+++ Gjyygle/src/main/java/Bibtex/BibtexGen.java
@@ -1,6 +1,7 @@
 package Bibtex;
 
 import gjyygle.BibtexTietokanta;
+import gjyygle.xml.XmlTietokanta;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -28,11 +29,17 @@ public class BibtexGen {
 //   pages = "389--404",
 //   year = 1991
 //   }
+//        Iterator it = mp.entrySet().iterator();
+//    while (it.hasNext()) {
+//        Map.Entry pairs = (Map.Entry)it.next();
+//        System.out.println(pairs.getKey() + " = " + pairs.getValue());
+//        it.remove(); // avoids a ConcurrentModificationException
+//    }
 
-    public void generate() {
+    public void generate(String tiedosto) {
         PrintWriter tiedot = null;
         try {
-            tiedot = new PrintWriter(new File("bibtex.bib"));
+            tiedot = new PrintWriter(new File(tiedosto));
         } catch (FileNotFoundException ex) {
             Logger.getLogger(BibtexGen.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -40,17 +47,18 @@ public class BibtexGen {
             tiedot.println("@" + entry.getType() + "{" + entry.getValue(BibtexField.ID));
             EnumMap<BibtexField, String> map = entry.getAllValues();
             for (EnumMap.Entry<BibtexField, String> arvot : map.entrySet()) {
-                if (arvot.getKey().equals(BibtexField.ID)) {
+                if (arvot.getKey().equals(BibtexField.ID) || arvot.getValue() == null) {
                     continue;
                 }
-                tiedot.println(arvot.getKey() + " : " + arvot.getValue());
+                tiedot.println(arvot.getKey() + " = \"" + arvot.getValue() + "\",");
             }
             tiedot.println("}");
         }
         tiedot.close();
     }
+    
 //    public static void main(String[] args) {
-//        BibtexGen gen = new BibtexGen();
-//        gen.generate();
+//        BibtexGen gen = new BibtexGen(new XmlTietokanta(new File("testXml.xml"), new File("tuukkatesti.xml")));
+//        gen.generate("bibtex.bib");
 //    }
 }
