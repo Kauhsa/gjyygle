462fa05d51a9dce9c60e2af6871379de51fa80dd
Tuukka T Peuraniemi <peuranie@dhp-24.cs.helsinki.fi>
2013-04-05 11:26:55 +0300

    Toimiva bibtexGen

--- Gjyygle/src/main/java/Bibtex/BibtexGen.java
+++ Gjyygle/src/main/java/Bibtex/BibtexGen.java
@@ -6,8 +6,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -44,21 +46,59 @@ public class BibtexGen {
             Logger.getLogger(BibtexGen.class.getName()).log(Level.SEVERE, null, ex);
         }
         for (BibtexEntry entry : lista) {
-            tiedot.println("@" + entry.getType() + "{" + entry.getValue(BibtexField.ID));
-            EnumMap<BibtexField, String> map = entry.getAllValues();
-            for (EnumMap.Entry<BibtexField, String> arvot : map.entrySet()) {
-                if (arvot.getKey().equals(BibtexField.ID) || arvot.getValue() == null) {
-                    continue;
+            EnumMap<BibtexField, String> map = palautaTulostettavaEnumMap(entry.getAllValues());
+            tiedot.println("@" + entry.getType() + "{" + entry.getValue(BibtexField.ID)+ ",");
+            Iterator entries = map.entrySet().iterator();
+            while (entries.hasNext()) {
+                EnumMap.Entry seuraavaArvo = (EnumMap.Entry) entries.next();
+//                if (seuraavaArvo.getValue() == null) {
+//                    continue;
+//                }
+                String rivi = seuraavaArvo.getKey() + " = \"" + seuraavaArvo.getValue() + "\"";
+                if (entries.hasNext()) {
+                    rivi += ",";
                 }
-                tiedot.println(arvot.getKey() + " = \"" + arvot.getValue() + "\",");
+                tiedot.println(rivi);
             }
             tiedot.println("}");
         }
         tiedot.close();
     }
+
+    public static void main(String[] args) {
+        BibtexGen gen = new BibtexGen(new XmlTietokanta(new File("testXml.xml"), new File("tuukkatesti.xml")));
+        gen.generate("bibtex.bib");
+    }
     
-//    public static void main(String[] args) {
-//        BibtexGen gen = new BibtexGen(new XmlTietokanta(new File("testXml.xml"), new File("tuukkatesti.xml")));
-//        gen.generate("bibtex.bib");
-//    }
+    private static EnumMap<BibtexField, String> palautaTulostettavaEnumMap(EnumMap<BibtexField, String> map) {
+        EnumMap<BibtexField, String> printMap = new EnumMap<BibtexField, String>(BibtexField.class);
+        for (EnumMap.Entry<BibtexField, String> arvot : map.entrySet()) {
+            if (arvot.getValue() != null) {
+                printMap.put(arvot.getKey(), arvot.getValue().toString());
+            }
+        }
+        return printMap;
+    }
 }
+
+
+//        for (BibtexEntry entry : lista) {
+//            tiedot.println("@" + entry.getType() + "{" + entry.getValue(BibtexField.ID)+ ",");
+//            EnumMap<BibtexField, String> map = entry.getAllValues();
+//            Iterator entries = map.entrySet().iterator();
+////            for (EnumMap.Entry<BibtexField, String> arvot : map.entrySet()) {
+//            while (entries.hasNext()) {
+//                EnumMap.Entry seuraavaArvo = (EnumMap.Entry) entries.next();
+//                if (seuraavaArvo.getValue() == null) {
+//                    continue;
+//                }
+////                if (arvo.getKey().equals(BibtexField.ID) || arvo.getValue() == null) { continue;}
+////                tiedot.println(seuraavaArvo.getKey() + " = \"" + seuraavaArvo.getValue() + "\"");
+//                String rivi = seuraavaArvo.getKey() + " = \"" + seuraavaArvo.getValue() + "\"";
+//                if (entries.hasNext()) {
+//                    rivi += ",";
+//                }
+//                tiedot.println(rivi);
+//            }
+//            tiedot.println("}");
+//        }
