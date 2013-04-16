6c506d81bc2ed0071d8693a50c664572e88aa06e
Mikko Tamminen <pahanaama@gmail.com>
2013-04-16 10:55:38 +0300

    fixed mock and test

--- Gjyygle/src/test/java/gjyygle/bibtex/BibtexTietokantaMock.java
+++ Gjyygle/src/test/java/gjyygle/bibtex/BibtexTietokantaMock.java
@@ -6,37 +6,43 @@ import gjyygle.bibtex.BibtexEntryType;
 import gjyygle.bibtex.BibtexField;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BibtexTietokantaMock implements BibtexTietokanta {
     ArrayList<BibtexEntry> defaultEntryt;
-    public BibtexTietokantaMock() throws ValidationException {
-        defaultEntryt = new ArrayList<BibtexEntry>();
-        BibtexEntry uusEntry = new BibtexEntry(BibtexEntryType.ARTICLE);
-        uusEntry.setValue(BibtexField.TITLE, "hieno artikkeli");
-        uusEntry.setValue(BibtexField.YEAR, "2013");
-        uusEntry.setValue(BibtexField.AUTHOR, "joku tyyppi");
-        uusEntry.setValue(BibtexField.JOURNAL, "jostain kirjasta kai");
-        uusEntry.setValue(BibtexField.ID, "aaa");
-        uusEntry.setValue(BibtexField.NOTE, "huom!");
-        uusEntry.setValue(BibtexField.VOLUME, "1");
-        defaultEntryt.add(uusEntry);
-        uusEntry = new BibtexEntry(BibtexEntryType.ARTICLE);
-        uusEntry.setValue(BibtexField.TITLE, "toinen hieno artikkeli");
-        uusEntry.setValue(BibtexField.YEAR, "2011");
-        uusEntry.setValue(BibtexField.AUTHOR, "joku toinen tyyppi");
-        uusEntry.setValue(BibtexField.JOURNAL, "jostain kirjasta melko varmasti");
-        uusEntry.setValue(BibtexField.MONTH, "5");
-        uusEntry.setValue(BibtexField.KEY, "?");
-        uusEntry.setValue(BibtexField.ID, "b");
-        uusEntry.setValue(BibtexField.PAGES, "101-103");
-        defaultEntryt.add(uusEntry);
-        uusEntry = new BibtexEntry(BibtexEntryType.ARTICLE);
-        uusEntry.setValue(BibtexField.TITLE, "huono artikkeli");
-        uusEntry.setValue(BibtexField.YEAR, "2010");
-        uusEntry.setValue(BibtexField.AUTHOR, "joku");
-        uusEntry.setValue(BibtexField.JOURNAL, "jostain");
-        uusEntry.setValue(BibtexField.ID, "c");
-        defaultEntryt.add(uusEntry);
+    public BibtexTietokantaMock(){
+        try {
+            defaultEntryt = new ArrayList<BibtexEntry>();
+            BibtexEntry uusEntry = new BibtexEntry(BibtexEntryType.ARTICLE);
+            uusEntry.setValue(BibtexField.TITLE, "hieno artikkeli");
+            uusEntry.setValue(BibtexField.YEAR, "2013");
+            uusEntry.setValue(BibtexField.AUTHOR, "joku tyyppi");
+            uusEntry.setValue(BibtexField.JOURNAL, "jostain kirjasta kai");
+            uusEntry.setValue(BibtexField.ID, "aaa");
+            uusEntry.setValue(BibtexField.NOTE, "huom!");
+            uusEntry.setValue(BibtexField.VOLUME, "1");
+            defaultEntryt.add(uusEntry);
+            uusEntry = new BibtexEntry(BibtexEntryType.ARTICLE);
+            uusEntry.setValue(BibtexField.TITLE, "toinen hieno artikkeli");
+            uusEntry.setValue(BibtexField.YEAR, "2011");
+            uusEntry.setValue(BibtexField.AUTHOR, "joku toinen tyyppi");
+            uusEntry.setValue(BibtexField.JOURNAL, "jostain kirjasta melko varmasti");
+            uusEntry.setValue(BibtexField.MONTH, "5");
+            uusEntry.setValue(BibtexField.KEY, "?");
+            uusEntry.setValue(BibtexField.ID, "b");
+            uusEntry.setValue(BibtexField.PAGES, "101-103");
+            defaultEntryt.add(uusEntry);
+            uusEntry = new BibtexEntry(BibtexEntryType.ARTICLE);
+            uusEntry.setValue(BibtexField.TITLE, "huono artikkeli");
+            uusEntry.setValue(BibtexField.YEAR, "2010");
+            uusEntry.setValue(BibtexField.AUTHOR, "joku");
+            uusEntry.setValue(BibtexField.JOURNAL, "jostain");
+            uusEntry.setValue(BibtexField.ID, "c");
+            defaultEntryt.add(uusEntry);
+        } catch (ValidationException ex) {
+            Logger.getLogger(BibtexTietokantaMock.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     @Override
     public void lisaaArtikkeli(BibtexEntry article) {
