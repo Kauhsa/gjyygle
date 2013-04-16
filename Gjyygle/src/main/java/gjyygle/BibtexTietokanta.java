cf9317db89bbf88787c5d762eeea2ed6f6247ede
Mikko Tamminen <pahanaama@gmail.com>
2013-04-16 10:37:44 +0300

    database throws

--- Gjyygle/src/main/java/gjyygle/BibtexTietokanta.java
+++ Gjyygle/src/main/java/gjyygle/BibtexTietokanta.java
@@ -1,11 +1,12 @@
 package gjyygle;
 
 import gjyygle.bibtex.BibtexEntry;
+import gjyygle.bibtex.ValidationException;
 import java.io.IOException;
 import java.util.List;
 
 public interface BibtexTietokanta {
-    public void lisaaArtikkeli(BibtexEntry article) throws IllegalArgumentException;
+    public void lisaaArtikkeli(BibtexEntry article) throws ValidationException;
     public List<BibtexEntry> listaaArtikkelit();
     public void tallenna() throws IOException;
 }
